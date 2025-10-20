28f04e454541cda8729199e0c517b242c0753c97
Vishal <vishaldewani500@gmail.com>
2025-10-20 16:16:43 +0500

    completed

--- Submissions/codeFront/SkillBridge/frontend/src/App.jsx
+++ Submissions/codeFront/SkillBridge/frontend/src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider, } from  'react-router-dom'
+import RootLayout from "./layout/RootLayout"
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import RefreshHandler from './components/RefreshHandler'
+import TeacherHandler from "./components/TeacherHandler"
+import CreateCourse from './pages/CreateCourse'
+import Profile from './pages/Profile'
+import Course from './pages/Course'
 function App() {
-  const [count, setCount] = useState(0)
 
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route element={<RefreshHandler />}>
+        <Route path='/' element={<RootLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          <Route path='/profile' element={<Profile />}/>
+          <Route path='/course/:id' element={<Course />}/>
+
+
+        <Route element={<TeacherHandler />}>
+          <Route path='/create' element={<CreateCourse />}/>
+        </Route>
+
+
+
+        </Route>
+        
+        <Route path='*' element={<NotFound />} />
+        </Route>
+      </>
+    )
+  )
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
